d5363bfb61d62a2646bd29a4e758ffc365f0c2fd
Jiwon Park <minerva1993@gmail.com>
2020-10-29 13:11:31 +0900

    tests for grouping jet cat

--- gather_and_merge_th1.py
+++ gather_and_merge_th1.py
@@ -3,8 +3,8 @@ import os, re
 
 base_dir = '/home2/minerva1993/HEPToolsFCNC/finalMVA/histos/2017/'
 #base_dir = '/home2/minerva1993/HEPToolsFCNC/finalMVA/histos/2018/'
-base_output_dir = 'histos_suitable_for_limits_200101v15_2017/'
-#base_output_dir = 'histos_suitable_for_limits_200101v2_2018/'
+base_output_dir = 'histos_suitable_for_limits_200101v43_2017/'
+#base_output_dir = 'histos_suitable_for_limits_200101v41_2018/'
 
 if not os.path.isdir(base_output_dir):
     os.mkdir(base_output_dir)
@@ -12,16 +12,13 @@ if not os.path.isdir(base_output_dir):
 # NB assumes DNN is in name of TH!
 
 coupling_strings = ['Hct', 'Hut']
-#jet_strings = ['j3', 'j4']
 jet_strings = ['j3b2', 'j3b3', 'j4b2', 'j4b3', 'j4b4']
-#training_strings = ['01', '02', '03', '04']
 training_strings = ['0101010101'] #ver of j3b2+j3b3+j4b2+j4b3+j4b4
 
 systematics_in_separated_rootfiles = ['hdamp', 'jec', 'jer', 'TuneCP5'] # first entry for the rootfiles with nominal and other syst TH1
 systematics_to_merge = {'lepton':'(\S*)__(el|mu)(\S*)',}#'btag':'(\S*)__(lf|hf|cferr)(\S*)'}
 
 # Derive first the list of processes
-#rootfile_dir = os.path.join(base_dir, coupling_strings[0] + "_" + jet_strings[0] + '_' + training_strings[0], 'post_process')
 rootfile_dir = os.path.join(base_dir, coupling_strings[0] + '_' + training_strings[0], 'post_process')
 process_list = [process.split('.')[0] for process in os.listdir(rootfile_dir) if process.endswith('.root') and not '__' in process]
 print 'Found following rootfiles : ', process_list
@@ -35,8 +32,6 @@ for training_string in training_strings:
         print "Writing %s"%output_file_name
         output_rootfile = ROOT.TFile(output_file_name, 'recreate')
         for coupling_string in coupling_strings:
-            #for jet_string in jet_strings:
-            #folder_name = coupling_string + "_" + jet_string + '_' + training_string
             folder_name = coupling_string + "_" + training_string
             rootfile_dir = os.path.join(base_dir, folder_name, 'post_process')
             rootfile_path = os.path.join(rootfile_dir, process + '.root')
@@ -47,19 +42,46 @@ for training_string in training_strings:
             median_dist = {}
             for th1_key in rootfile.GetListOfKeys():
                 th1_name = th1_key.GetName()
-                if not 'j3b2' in th1_name: continue
                 if not 'DNN' in th1_name: continue
                 if '__' in th1_name: continue
-                th1 = rootfile.Get(th1_name)
-                th1a = rootfile.Get(th1_name.replace('j3b2','j3b3'))
-                th1b = rootfile.Get(th1_name.replace('j3b2','j4b2'))
-                th1c = rootfile.Get(th1_name.replace('j3b2','j4b3'))
-                th1d = rootfile.Get(th1_name.replace('j3b2','j4b4'))
-                th1.Add(th1a,1.0)
-                th1.Add(th1b,1.0)
-                th1.Add(th1c,1.0)
-                th1.Add(th1d,1.0)
-                #new_th1_name = coupling_string + '_' + jet_string + '_' + th1_name
+                #Merge into one histo
+                #if not 'j3b2' in th1_name: continue
+                #th1 = rootfile.Get(th1_name)
+                #th1a = rootfile.Get(th1_name.replace('j3b2','j3b3'))
+                #th1b = rootfile.Get(th1_name.replace('j3b2','j4b2'))
+                #th1c = rootfile.Get(th1_name.replace('j3b2','j4b3'))
+                #th1d = rootfile.Get(th1_name.replace('j3b2','j4b4'))
+                #th1.Add(th1a,1.0)
+                #th1.Add(th1b,1.0)
+                #th1.Add(th1c,1.0)
+                #th1.Add(th1d,1.0)
+
+                #Merge w.r.t. b jets
+                #if 'j3b2' in th1_name:
+                #    th1 = rootfile.Get(th1_name)
+                #    th1a = rootfile.Get(th1_name.replace('j3b2','j4b2'))
+                #    th1.Add(th1a,1.0)
+                #elif 'j3b3' in th1_name:
+                #    th1 = rootfile.Get(th1_name)
+                #    th1a = rootfile.Get(th1_name.replace('j3b3','j4b3'))
+                #    th1b = rootfile.Get(th1_name.replace('j3b3','j4b4'))
+                #    th1.Add(th1a,1.0)
+                #    th1.Add(th1b,1.0)
+                #else: continue
+
+                #Merge w.r.t. jets
+                if 'j3b2' in th1_name:
+                    th1 = rootfile.Get(th1_name)
+                    th1a = rootfile.Get(th1_name.replace('j3b2','j3b3'))
+                    th1.Add(th1a,1.0)
+                elif 'j4b2' in th1_name:
+                    th1 = rootfile.Get(th1_name)
+                    th1a = rootfile.Get(th1_name.replace('j4b2','j4b3'))
+                    th1b = rootfile.Get(th1_name.replace('j4b2','j4b4'))
+                    th1.Add(th1a,1.0)
+                    th1.Add(th1b,1.0)
+                else: continue
+
                 new_th1_name = coupling_string + '_' + th1_name
                 th1.SetName(new_th1_name)
 
@@ -79,18 +101,45 @@ for training_string in training_strings:
 
             for th1_key in rootfile.GetListOfKeys():
                 th1_name = th1_key.GetName()
-                if not 'j3b2' in th1_name: continue
                 if not 'DNN' in th1_name: continue
-                th1 = rootfile.Get(th1_name)
-                th1a = rootfile.Get(th1_name.replace('j3b2','j3b3'))
-                th1b = rootfile.Get(th1_name.replace('j3b2','j4b2'))
-                th1c = rootfile.Get(th1_name.replace('j3b2','j4b3'))
-                th1d = rootfile.Get(th1_name.replace('j3b2','j4b4'))
-                th1.Add(th1a,1.0)
-                th1.Add(th1b,1.0)
-                th1.Add(th1c,1.0)
-                th1.Add(th1d,1.0)
-                #new_th1_name = coupling_string + '_' + jet_string + '_' + th1_name
+                #Merge into one histo
+                #if not 'j3b2' in th1_name: continue
+                #th1 = rootfile.Get(th1_name)
+                #th1a = rootfile.Get(th1_name.replace('j3b2','j3b3'))
+                #th1b = rootfile.Get(th1_name.replace('j3b2','j4b2'))
+                #th1c = rootfile.Get(th1_name.replace('j3b2','j4b3'))
+                #th1d = rootfile.Get(th1_name.replace('j3b2','j4b4'))
+                #th1.Add(th1a,1.0)
+                #th1.Add(th1b,1.0)
+                #th1.Add(th1c,1.0)
+                #th1.Add(th1d,1.0)
+
+                #Merge w.r.t. b jets
+                #if 'j3b2' in th1_name:
+                #    th1 = rootfile.Get(th1_name)
+                #    th1a = rootfile.Get(th1_name.replace('j3b2','j4b2'))
+                #    th1.Add(th1a,1.0)
+                #elif 'j3b3' in th1_name:
+                #    th1 = rootfile.Get(th1_name)
+                #    th1a = rootfile.Get(th1_name.replace('j3b3','j4b3'))
+                #    th1b = rootfile.Get(th1_name.replace('j3b3','j4b4'))
+                #    th1.Add(th1a,1.0)
+                #    th1.Add(th1b,1.0)
+                #else: continue
+
+                #Merge w.r.t. jets
+                if 'j3b2' in th1_name:
+                    th1 = rootfile.Get(th1_name)
+                    th1a = rootfile.Get(th1_name.replace('j3b2','j3b3'))
+                    th1.Add(th1a,1.0)
+                elif 'j4b2' in th1_name:
+                    th1 = rootfile.Get(th1_name)
+                    th1a = rootfile.Get(th1_name.replace('j4b2','j4b3'))
+                    th1b = rootfile.Get(th1_name.replace('j4b2','j4b4'))
+                    th1.Add(th1a,1.0)
+                    th1.Add(th1b,1.0)
+                else: continue
+
                 new_th1_name = coupling_string + '_' + th1_name
                 th1.SetName(new_th1_name)
 
@@ -125,18 +174,45 @@ for training_string in training_strings:
                     output_rootfile.cd()
                     for th1_key in rootfile.GetListOfKeys():
                         th1_name = th1_key.GetName()
-                        if not 'j3b2' in th1_name: continue
                         if not 'DNN' in th1_name: continue
-                        th1 = rootfile.Get(th1_name)
-                        th1a = rootfile.Get(th1_name.replace('j3b2','j3b3'))
-                        th1b = rootfile.Get(th1_name.replace('j3b2','j4b2'))
-                        th1c = rootfile.Get(th1_name.replace('j3b2','j4b3'))
-                        th1d = rootfile.Get(th1_name.replace('j3b2','j4b4'))
-                        th1.Add(th1a,1.0)
-                        th1.Add(th1b,1.0)
-                        th1.Add(th1c,1.0)
-                        th1.Add(th1d,1.0)
-                        #new_th1_name = coupling_string + '_' + jet_string + '_' + th1_name + '__' + syst + var
+                        #Merge into one histo 
+                        #if not 'j3b2' in th1_name: continue
+                        #th1 = rootfile.Get(th1_name)
+                        #th1a = rootfile.Get(th1_name.replace('j3b2','j3b3'))
+                        #th1b = rootfile.Get(th1_name.replace('j3b2','j4b2'))
+                        #th1c = rootfile.Get(th1_name.replace('j3b2','j4b3'))
+                        #th1d = rootfile.Get(th1_name.replace('j3b2','j4b4'))
+                        #th1.Add(th1a,1.0)
+                        #th1.Add(th1b,1.0)
+                        #th1.Add(th1c,1.0)
+                        #th1.Add(th1d,1.0)
+
+                        #Merge w.r.t. b jets
+                        #if 'j3b2' in th1_name:
+                        #    th1 = rootfile.Get(th1_name)
+                        #    th1a = rootfile.Get(th1_name.replace('j3b2','j4b2'))
+                        #    th1.Add(th1a,1.0)
+                        #elif 'j3b3' in th1_name:
+                        #    th1 = rootfile.Get(th1_name)
+                        #    th1a = rootfile.Get(th1_name.replace('j3b3','j4b3'))
+                        #    th1b = rootfile.Get(th1_name.replace('j3b3','j4b4'))
+                        #    th1.Add(th1a,1.0)
+                        #    th1.Add(th1b,1.0)
+                        #else: continue
+
+                        #Merge w.r.t. jets
+                        if 'j3b2' in th1_name:
+                            th1 = rootfile.Get(th1_name)
+                            th1a = rootfile.Get(th1_name.replace('j3b2','j3b3'))
+                            th1.Add(th1a,1.0)
+                        elif 'j4b2' in th1_name:
+                            th1 = rootfile.Get(th1_name)
+                            th1a = rootfile.Get(th1_name.replace('j4b2','j4b3'))
+                            th1b = rootfile.Get(th1_name.replace('j4b2','j4b4'))
+                            th1.Add(th1a,1.0)
+                            th1.Add(th1b,1.0)
+                        else: continue
+
                         new_th1_name = coupling_string + '_' + th1_name + '__' + syst + var
                         th1.SetName(new_th1_name)
                         th1.Write()
