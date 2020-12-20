0efc3dd529128dde823f9684d4042735a02c973e
Jiwon Park <minerva1993@gmail.com>
2020-12-20 18:05:18 +0900

    more space for pull plots

--- printPulls.py
+++ printPulls.py
@@ -5,7 +5,9 @@ rootfilename = sys.argv[1]
 rootfile = ROOT.TFile(rootfilename)
 canvas = ROOT.TCanvas()
 rootfile.GetObject('nuisances', canvas)
-canvas.SetBottomMargin(0.2)
+canvas.SetBottomMargin(0.3)
+canvas.SetRightMargin(0.03)
+canvas.SetLeftMargin(0.07)
 
 print rootfilename
 canvas.Print(rootfilename.replace(".root", ".png"))
