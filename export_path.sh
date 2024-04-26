3b4e5718dc73210f60e19959396cea95b71024fd
Jiwon Park <jiwonpark1993@gmail.com>
2024-04-26 19:26:02 +0900

    remove old FCNC files

--- export_path.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=`echo $PYTHONPATH | sed -e 's/:\/cvmfs\/cms\.cern\.ch\/slc6_amd64_gcc530\/external\/py2-pandas\/0\.17\.1-giojec7\/lib\/python2\.7\/site-packages//'`
-
-export PYTHONPATH=/home/minerva1993/CMSSW_8_1_0/external/py2_install/lib/python2.7/site-packages:$PYTHONPATH
