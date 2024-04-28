7bb2873cbe8e96d36dee204af116a3b25fca24f0
Jiwon Park <jiwonpark1993@gmail.com>
2024-04-28 23:57:03 +0900

    run failed fit in the end

--- run_everything_oneYear.sh
+++ run_everything_oneYear.sh
@@ -5,8 +5,8 @@ python run_all_limits.py $datacardFolder
 python plotLimitsPerCategory.py -limitfolder $datacardFolder
 python printLimitLatexTable.py $datacardFolder > out_$datacardFolder
 python run_all_impacts.py $datacardFolder
-python run_all_gatherFailedFits.py $datacardFolder
 python plot_syst.py $datacardFolder
+python run_all_gatherFailedFits.py $datacardFolder
 
 #python run_all_postfits.py $datacardFolder
 #python printPostfitLatexTable.py $datacardFolder
